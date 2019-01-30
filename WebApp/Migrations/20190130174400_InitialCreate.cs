eaed32dc5375aff91e1ec2b4f8278bd08c1acf6e
Kirill Ezhov <kirillezhov154@gmail.com>
2019-01-30 21:12:45 +0300

    20190130

---
+++ WebApp/Migrations/20190130174400_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Type = table.Column<string>(nullable: true),
+                    Model = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false),
+                    Rack = table.Column<byte>(nullable: false),
+                    Cell = table.Column<byte>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ WebApp/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        public byte Rack { get; set; }
+        public byte Cell { get; set; }
+    }
+}
